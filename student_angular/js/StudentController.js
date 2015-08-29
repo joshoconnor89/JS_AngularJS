1521460e81f98322e34b59aecc06c63a4c88e621
joshoconnor89 <joshoconnor89@gmail.com>
2015-08-29 09:08:07 -0700

    Angular JS
    Angular JS examples from UCSD’s Mobile App Development course.

---
+++ student_angular/js/StudentController.js
@@ -0,0 +1,8 @@
+studentRoster.controller('StudentCtrl',function StudentCtrl($scope){
+   $scope.students =[
+     {name:"Kris Secor"},
+     {name:"Jane Klimchak"},
+     {name:"Renata Stahl"},
+     {name:"Sanford Schmidt"}
+   ]
+});
